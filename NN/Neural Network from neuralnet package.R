032d970ab762f542dfb55d3ed6ff546e19a16078
andrewmanigaultw <andrewmanigaultw@gmail.com>
2022-10-14 08:14:29 -0400

    tidied up NN scripts

--- NN/Neural Network from neuralnet package.R
+++ NN/Neural Network from neuralnet package.R
@@ -11,7 +11,6 @@ load("NN_data.RData")
 library(neuralnet)
 
 #
-dim(data)
 data <- as.data.frame(data)
 data[,-1] <- apply(data[,-1], 2, as.numeric) 
 
@@ -20,36 +19,47 @@ data_shuffled <- data[sample(nrow(data)), ]
 
 test <- data_shuffled[0:2000, ]
 
+getNN <- function(data, filename){
+  start <- Sys.time()
+  nn <- neuralnet::neuralnet(nextMoveDir ~., data, hidden = 10, linear.output = FALSE, threshold = .1)
+  save(nn, file = paste0(filename, ".RData"))
+  end <- Sys.time()
+  print(end - start)
+}
 
-train <- data_shuffled[2001:4000, ]
-nn <- neuralnet(nextMoveDir ~., train, hidden = 10, linear.output = FALSE)
-save(nn, file = "nn1.RData")
-
-train <- data_shuffled[2001:8000, ]
-nn <- neuralnet(nextMoveDir ~., train, hidden = 10, linear.output = FALSE)
-save(nn, file = "nn2.RData")
-
-train <- data_shuffled[2001:16000, ]
-nn <- neuralnet(nextMoveDir ~., train, hidden = 10, linear.output = FALSE)
-save(nn, file = "nn3.RData")
-
-train <- data_shuffled[2001:32000, ]
-nn <- neuralnet(nextMoveDir ~., train, hidden = 10, linear.output = FALSE)
-save(nn, file = "nn4.RData")
+getNN(data_shuffled[2001:3000, ], "nn1")
+getNN(data_shuffled[2001:4000, ], "nn2")
+getNN(data_shuffled[2001:6000, ], "nn3")
+getNN(data_shuffled[2001:10000, ], "nn4")
 
 
 
-pred <- predict(nn, test[,-1])
-pred2 <- apply(pred, 1, function(x) which(x == max(x))[1])
 getStringPred <- Vectorize(vectorize.args = "x",
-                 FUN = function(x){
-                   switch (x,
-                           "1" = "down",
-                           "2" = "left",
-                           "3" = "right",
-                           "4" = "up")
-                 })
-pred3 <- getStringPred(pred2)
+                           FUN = function(x){
+                             switch (x,
+                                     "1" = "down",
+                                     "2" = "left",
+                                     "3" = "right",
+                                     "4" = "up")
+                           })
 
-caret::confusionMatrix(table(prediction = pred3, reference = test[,1]))
+evaluatePred <- function(nn, truth){
+  pred <- predict(nn, test[,-1])
+  pred2 <- apply(pred, 1, function(x) which(x == max(x))[1])
+  pred3 <- getStringPred(pred2)
+  
+  caret::confusionMatrix(table(prediction = pred3, reference = test[,1]))
+}
 
+#
+load("nn1.RData")
+evaluatePred(nn, test[,-1])
+
+load("nn2.RData")
+evaluatePred(nn, test[,-1])
+
+load("nn3.RData")
+evaluatePred(nn, test[,-1])
+
+load("nn4.RData")
+evaluatePred(nn, test[,-1])
